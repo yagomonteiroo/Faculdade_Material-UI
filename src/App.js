82773c67c20a63baba4e3cd16b602a9b15683a2c
yagomonteiroo <yago.monteiroo@gmail.com>
2024-05-31 21:19:26 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./components/header";
+import ListarTarefa from "./pages/tarefa/ListarTarefa";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <ListarTarefa />
+    </div>
+  );
+}
+
+export default App;
